2482c4da47eff493bab228d454c5751e366cd8e9
301325509 <301325509@PR-A146-04>
2024-01-25 19:09:06 -0500

    Add project files.

---
+++ COMP123 Weekly Discussion/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP123_Weekly_Discussion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
